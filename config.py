c4bc8b85a935f4756605c143c4c7589218a87317
monotoha <josebenitezdoblado@hotmail.com>
2019-05-31 15:31:21 +0200

    Update config.py
    Added Cassandra variables to config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-DATA_BACKEND = 'mongodb'
+DATA_BACKEND = 'cassandra'
 
 # mongo configuration
 
@@ -12,3 +12,9 @@ MONGO_PORT = '27017'
 MONGO_DB = 'test'
 MONGO_URI = \
     'mongodb://'+MONGO_IP+':'+MONGO_PORT+'/'+MONGO_DB
+
+# Cassandra configuration
+
+CASSANDRA_IP = 'localhost'
+CASSANDRA_PORT = 9042
+CASSANDRA_KEYSPACE = 'wine_review'
